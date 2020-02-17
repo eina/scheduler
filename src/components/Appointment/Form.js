568d392bdbc9f9cb26010416302568e1bc8fdf40
Eina Onting <einers@outlook.com>
2020-02-17 15:51:31 -0800

    create Form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+
+const Form = props => {
+  const [interviewer, setInterviewer] = useState(0);
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+          This must be a controlled component
+        */
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={props.interviewer ? props.interviewer : interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={props.onCancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Form;
--- stories/index.js
+++ stories/index.js
@@ -19,6 +19,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,4 +146,15 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />);
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+  .add("Edit Form", () => (
+    <Form
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create Form", () => (
+    <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />
+  ));
