71c2ae09e576ea9b29aa398da51aec9c02bfaf8d
Eina Onting <einers@outlook.com>
2020-02-17 11:22:32 -0800

    implement <Button /> interfaces

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-  return <></>;
+  const { disabled, onClick } = props;
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
+      {props.children}
+    </button>
+  );
 }
