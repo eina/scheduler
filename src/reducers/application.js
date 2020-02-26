85be8e5d1d1e2be3f79db7da3125ed374a2a971e
Eina Onting <einers@outlook.com>
2020-02-25 16:10:49 -0800

    test circleci with breaking changes

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,6 +1,6 @@
 /*eslint indent: ["error", 2, { "SwitchCase": 1 }]*/
 
-export const SET_DAY = "SET_DAY";
+// export const SET_DAY = "SET_DAY";
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_INTERVIEW = "SET_INTERVIEW";
 
